ef01f85421ecaf26b9648c950b389c17d2d3e74a
Mike Green <mike.is.green@gmail.com>
2009-05-25 22:13:44 -0400

    need to test, but wrote the methods required for random quote retrieval

--- jquery.quotate.js
+++ jquery.quotate.js
@@ -2,15 +2,50 @@
 	
 	$.fn.quotate = function(options) {
 		
+		var quoteList;
+		
 		var defaults = {
 			type: "json",
-			interval: 10
+			interval: 10,
+			quoteClass: "quote",
+			authorClass: "author"
 		};
 		var options = $.extend(defaults, options);
 		
+		// private dataReady function
+		function dataReady() {
+			var interval = options.interval * 1000;
+			$(this).everyTime(interval, function() {
+				$(this).html('<div class="'+options.quoteClass+'"></div><div class="'+options.authorClass+'"></div>'); // clear out container element
+				if (quoteList.length > 0) {
+					var limit = quoteList.length;
+					var randIndex = Math.floor(Math.random()*limit+1);
+					var randomQuote = quoteList[randIndex];
+					$(this).find("."+options.quoteClass).html(randomQuote.quote);
+					$(this).find("."+options.authorClass).html(randomQuote.author);
+				} else {
+					return false;
+				}
+			});
+		}
+		
 		return this.each(function() {
-			obj = $(this);
-			
+			// Check if we have the jQuery Timers plugin. If not, go no further.
+			if (typeof(everyTime) == "function") {
+				if (typeof(options.type) == "undefined") {
+					
+				} else {
+					if (options.type == "json") {
+						
+					} else if (options.type == "xml") {
+						
+					} else {
+						return false;
+					}
+				}
+			} else {
+				return false;
+			}
 		});
 	};
 	
