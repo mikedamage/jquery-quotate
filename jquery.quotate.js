355705fe28e7bcdcc1c6728cc21c86fde46c5259
Mike Green <mike.is.green@gmail.com>
2009-05-25 20:50:10 -0400

    added jquery 1.3.2 and jquery.timers plugin v1.1.2

---
+++ jquery.quotate.js
@@ -0,0 +1,17 @@
+(function($) {
+	
+	$.fn.quotate = function(options) {
+		
+		var defaults = {
+			type: "json",
+			interval: 10
+		};
+		var options = $.extend(defaults, options);
+		
+		return this.each(function() {
+			obj = $(this);
+			
+		});
+	};
+	
+}) (jQuery);
